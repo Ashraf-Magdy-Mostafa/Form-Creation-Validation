3bbdbeff34b61101fd554de0ef53551d53390043
ashraf magdy <ashrafmagdy926@gmail.com>
2025-11-27 14:06:41 +0200

    Task 0 - edited to remove arrow functrion

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", function () {
   console.log("Dom Loaded");
 
   const form = document.getElementById("registration-form");
